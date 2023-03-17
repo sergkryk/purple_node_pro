69faf1e3357042bc1a226c2429a2b6483c3adf8f
sergkryk <sergkryk@ya.ru>
2023-03-17 16:38:08 +0300

    промежуточный комит

--- exercises/8-1 -weather-api/routes/weather.js
+++ exercises/8-1 -weather-api/routes/weather.js
@@ -1,4 +1,12 @@
+import axios from "axios";
+
 export default async function router(req, res, next) {
-  const city = req.query.city || 'Not specified';
-  res.json({message: city});
+  const city = req.query.city;
+  console.log(city);
+  if (!city) {
+    res.json({message: "Укажите город в запросе"});
+  }
+  const cityInfo = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.WEATHER_API_KEY}`);
+  console.log(cityInfo);
+  res.json({ message: city });
 }
